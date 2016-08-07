c5aaebcf9978617e93cee25de4b21ebb75f80669
umberto simonato <u.simonato@libero.it>
2016-08-07 22:51:10 +0200

    Test014

--- common/models/message.js
+++ common/models/message.js
@@ -1,8 +1,8 @@
 var Twitter = require('twitter');
 module.exports = function(Message) 
 { //Use the environment variables in production
-  //var client = new Twitter({ consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, });
-  var client = new Twitter({ consumer_key: 'bsx4tgBYfslT5TzmjqJqxifk4', consumer_secret: 'aljTXQ8tVs8R8AIdV48flBu7QIv4R6MWWuZ7hL4hewFY9F6KyT', access_token_key: '762367617407545344-DxgiPtDgP4Ou5owzhO3drxlkSHixNVX', access_token_secret: 'i9umH6w71Tkj61khvCZ9jKuJ5KoVLLtJXZrCoITEDTfES',});
+  var client = new Twitter({ consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, });
+  //var client = new Twitter({ consumer_key: 'bsx4tgBYfslT5TzmjqJqxifk4', consumer_secret: 'aljTXQ8tVs8R8AIdV48flBu7QIv4R6MWWuZ7hL4hewFY9F6KyT', access_token_key: '762367617407545344-DxgiPtDgP4Ou5owzhO3drxlkSHixNVX', access_token_secret: 'i9umH6w71Tkj61khvCZ9jKuJ5KoVLLtJXZrCoITEDTfES',});
 //Available methods : // client.get(path, params, callback); // client.post(path, params, callback); // client.stream(path, params, callback);
 
    Message.afterRemote('create', function (ctx, message, next)
@@ -15,7 +15,7 @@ module.exports = function(Message)
       //client.post('statuses/update', {status: "Maria2"}, function(error, tweet, response){ if(error) console.log(error); console.log(tweet)
       // Tweet body.
       //console.log(response);
-      client.post('/direct_messages/new.json', {screen_name: 'GRS_BREGANZE', 'text': 'Time:' + message.time + ' Msg Alarm from ' + message.device + 'data' + +message.data}, function(error, tweet, response){ if(error) console.log(error); console.log(tweet)
+      client.post('/direct_messages/new.json', {screen_name: 'GRS_BREGANZE', 'text': 'Time:' + message.time + ' Msg Alarm from ' + message.device + ' data' + +message.data}, function(error, tweet, response){ if(error) console.log(error); console.log(tweet)
       // Tweet body.
       console.log(response);
 

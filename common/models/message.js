dc692dce147af2e258b7f4cee68d4f3b625db580
umberto simonato <u.simonato@libero.it>
2016-08-07 23:58:13 +0200

    Test020

--- common/models/message.js
+++ common/models/message.js
@@ -1,4 +1,5 @@
 var Twitter = require('twitter');
+require('date-utils');
 module.exports = function(Message)
 { //Use the environment variables in production
   var client = new Twitter({ consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, });

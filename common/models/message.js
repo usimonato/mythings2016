dc188d92d97b1931b52310fbd898bb5998dbe78c
umberto simonato <u.simonato@libero.it>
2016-08-07 12:05:17 +0200

    Init

---
+++ common/models/message.js
@@ -0,0 +1,24 @@
+var Twitter = require('twitter'); 
+module.exports = function(Message) 
+{ //Use the environment variables in production 
+  //var client = new Twitter({ consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, });
+  var client = new Twitter({ consumer_key: 'eE7WLUbqT8Xlk1vXwBWeHdjSF', consumer_secret: 'oPePewnIGB0skdpMqbFyYcYfQumfBMV3ga6fOlUt7lL9pcVLIO', access_token_key: '757098722538979328-niLdd2zA2MfJyWRTS8Zt5ZYq8kPKHLb', access_token_secret: 'AY0I0lfVzxImKxLbqNQCPVsrOfAVhIgt4jZcI9Yq61F0H',});
+//Available methods : // client.get(path, params, callback); // client.post(path, params, callback); // client.stream(path, params, callback);
+
+   Message.afterRemote('create', function (ctx, message, next)
+   { 
+      console.log('> testing afterRemote function'); 
+      console.log('time : '+message.time); 
+      console.log('device id : '+message.device);
+      console.log('data : '+message.data);
+
+      //client.post('statuses/update', {status: "Maria2"}, function(error, tweet, response){ if(error) console.log(error); console.log(tweet)
+      // Tweet body.
+      //console.log(response);
+      client.post('/direct_messages/new.json', {screen_name: 'GRS_BREGANZE', 'text':'This is easy.'}, function(error, tweet, response){ if(error) console.log(error); console.log(tweet)
+      // Tweet body.
+      console.log(response);
+
+      // Raw response object. 
+      }); next(); });
+};

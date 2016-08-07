01d748e275b1128ef77d04ab97d8244d3dfd4dc5
umberto simonato <u.simonato@libero.it>
2016-08-07 19:22:03 +0200

    Test08

--- common/models/message.js
+++ common/models/message.js
@@ -22,7 +22,7 @@ module.exports = function(Message)
   //var client = new Twitter({ consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, });
   var client = new Twitter({ consumer_key: 'eE7WLUbqT8Xlk1vXwBWeHdjSF', consumer_secret: 'oPePewnIGB0skdpMqbFyYcYfQumfBMV3ga6fOlUt7lL9pcVLIO', access_token_key: '757098722538979328-niLdd2zA2MfJyWRTS8Zt5ZYq8kPKHLb', access_token_secret: 'AY0I0lfVzxImKxLbqNQCPVsrOfAVhIgt4jZcI9Yq61F0H',});
 //Available methods : // client.get(path, params, callback); // client.post(path, params, callback); // client.stream(path, params, callback);
-   var dataora = '2016-08-07, 8:35';
+  // var dataora = '2016-08-07, 8:35';
    Message.afterRemote('create', function (ctx, message, next)
    {
       console.log('> testing afterRemote function');

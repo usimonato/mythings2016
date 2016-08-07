4af8557f24c891da1d5e9e68c8ee0d547a02c094
umberto simonato <u.simonato@libero.it>
2016-08-07 22:43:59 +0200

    Test13

--- common/models/message.js
+++ common/models/message.js
@@ -1,13 +1,13 @@
 var Twitter = require('twitter');
 module.exports = function(Message) 
-{ //Use the environment variables in production 
+{ //Use the environment variables in production
   //var client = new Twitter({ consumer_key: process.env.TWITTER_CONSUMER_KEY, consumer_secret: process.env.TWITTER_CONSUMER_SECRET, access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY, access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET, });
-  var client = new Twitter({ consumer_key: 'eE7WLUbqT8Xlk1vXwBWeHdjSF', consumer_secret: 'oPePewnIGB0skdpMqbFyYcYfQumfBMV3ga6fOlUt7lL9pcVLIO', access_token_key: '757098722538979328-niLdd2zA2MfJyWRTS8Zt5ZYq8kPKHLb', access_token_secret: 'AY0I0lfVzxImKxLbqNQCPVsrOfAVhIgt4jZcI9Yq61F0H',});
+  var client = new Twitter({ consumer_key: 'bsx4tgBYfslT5TzmjqJqxifk4', consumer_secret: 'aljTXQ8tVs8R8AIdV48flBu7QIv4R6MWWuZ7hL4hewFY9F6KyT', access_token_key: '762367617407545344-DxgiPtDgP4Ou5owzhO3drxlkSHixNVX', access_token_secret: 'i9umH6w71Tkj61khvCZ9jKuJ5KoVLLtJXZrCoITEDTfES',});
 //Available methods : // client.get(path, params, callback); // client.post(path, params, callback); // client.stream(path, params, callback);
 
    Message.afterRemote('create', function (ctx, message, next)
    { 
-      console.log('> testing afterRemote function'); 
+      console.log('> testing afterRemote function');
       console.log('time : '+message.time); 
       console.log('device: '+message.device);
       console.log('data : '+message.data);

9b50cee721d11bd6103b49fddd66dbb9c74e9875
umberto simonato <u.simonato@libero.it>
2016-08-17 11:22:21 +0200

    test182

--- common/models/message.js
+++ common/models/message.js
@@ -52,7 +52,7 @@ function getLocationData(callback) {
 }
 
 var myCallback = function(err, data) {
-  if (err) throw err; // Check for the error and throw if it exists.
+  if (err) {console.log(err);}; // Check for the error and throw if it exists.
   console.log('got data: '+data); // Otherwise proceed as usual.
 };
 
